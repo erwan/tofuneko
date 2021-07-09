c7f0897be0de49a9d649517c9e114b65c56818a3
Erwan Loisant <erwan@loisant.com>
2021-07-09 21:05:15 +0200

    Kento

--- game.py
+++ game.py
@@ -4,46 +4,93 @@ import pgzrun
 WIDTH = 960
 HEIGHT = 640
 
-speed = 4
+speed = 6
+enemySpeed = 2
+enemySpeed2 = 2
 
 neko = Actor('front')
 neko.pos = WIDTH / 2, HEIGHT / 2
 
-crate = Actor('crate')
-crate.pos = 200, HEIGHT / 2
+enemy = Actor('enemy')
+enemy.pos = 64, HEIGHT / 2
 
+enemy2 = Actor('enemy6')
+enemy2.pos = WIDTH / 2, 64
 
+retour = False
+retour2 = False
 def draw():
     screen.blit('map', (0, 0))
     neko.draw()
-    crate.draw()
-
-
+    enemy.draw()
+    enemy2.draw()
+    
 def update():
-    collid = neko.colliderect(crate)
+    joueur()
+    enemi()
+    enemi2()
+def joueur():
     # Keyboard input
     if keyboard.left:
         neko.x -= speed
         neko.image = 'left'
-        if collid:
-            crate.x -= speed
     if keyboard.right:
         neko.x += speed
         neko.image = 'right'
-        if collid:
-            crate.x += speed
     if keyboard.up:
         neko.y -= speed
         neko.image = 'back'
-        if collid:
-            crate.y -= speed
     if keyboard.down:
         neko.y += speed
         neko.image = 'front'
-        if collid:
-            crate.y += speed
     if keyboard.space:
         sounds.jump.play()
+
+
+def enemi():
+    global retour
+    if neko.colliderect(enemy):
+       enemy.image = 'enemy3' 
+    else:
+       enemy.image = 'enemy'
+    if enemy.x>896:
+        retour = True
+    if enemy.x<64:
+        retour = False
+    
+    if retour:
+       enemy.x -= enemySpeed
+    else:
+       enemy.x += enemySpeed
+
+
+def enemi2():
+    global retour2
+    if neko.colliderect(enemy2):
+       enemy2.image = 'enemy5'
+    else:
+       enemy2.image = 'enemy6' 
+    if enemy2.y>576:
+        retour2 = True
+    if enemy2.y<64:
+        retour2 = False
+       
+    if retour2:
+       enemy2.y -= enemySpeed2
+    else:
+       enemy2.y += enemySpeed2
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 pgzrun.go()
