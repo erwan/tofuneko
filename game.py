820fd595e7b3290ff3ff837840cb911974b89796
Erwan Loisant <erwan@loisant.com>
2021-07-12 13:40:02 +0200

    progress

--- game.py
+++ game.py
@@ -6,7 +6,8 @@ HEIGHT = 640
 
 speed = 6
 enemySpeed = 2
-enemySpeed2 = 2
+enemySpeed2 = 3
+enemySpeed3 = 1
 
 neko = Actor('front')
 neko.pos = WIDTH / 2, HEIGHT / 2
@@ -17,18 +18,28 @@ enemy.pos = 64, HEIGHT / 2
 enemy2 = Actor('enemy6')
 enemy2.pos = WIDTH / 2, 64
 
+boss = Actor('boss')
+boss.pos = WIDTH / 2, HEIGHT / 2
+
+enemy3 = Actor('enemy8')
+enemy3.pos = 896, HEIGHT / 2
+
 retour = False
 retour2 = False
+retour3 = False
 def draw():
     screen.blit('map', (0, 0))
     neko.draw()
     enemy.draw()
     enemy2.draw()
-    
+    boss.opacity = 0
+    enemy3.draw()
 def update():
     joueur()
     enemi()
     enemi2()
+    boss1()
+    enemi3()
 def joueur():
     # Keyboard input
     if keyboard.left:
@@ -41,8 +52,8 @@ def joueur():
         neko.y -= speed
         neko.image = 'back'
     if keyboard.down:
-        neko.y += speed
-        neko.image = 'front'
+       neko.y += speed
+       neko.image = 'front'
     if keyboard.space:
         sounds.jump.play()
 
@@ -80,17 +91,26 @@ def enemi2():
     else:
        enemy2.y += enemySpeed2
 
-
-
-
-
-
-
-
-
-
-
-
+def boss1():
+    if neko.colliderect(enemy2):
+       boss.opacity = 1
+       
+ 
+def enemi3():
+    global retour3
+    if neko.colliderect(enemy3):
+       enemy3.image = 'enemy9'
+    else:
+       enemy3.image = 'enemy8'
+    if enemy3.x<64:
+       retour3 = True
+    if enemy3.x>896:
+       retour3 = False 
+    
+    if retour3:
+       enemy3.x += enemySpeed3
+    else:
+       enemy3.x -= enemySpeed3 
 
 
 pgzrun.go()
